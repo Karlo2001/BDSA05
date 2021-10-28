247a2a66895d4ee33af75277284781c82063fe44
Karlo2001 <kabm@itu.dk>
2021-10-28 17:34:16 +0200

    Remove dead code

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -14,7 +14,7 @@ namespace GildedRose
             
             Items = new List<Item>
             {
-                new() {QualityChange = NormalQuality, Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new() {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new() {QualityChange = AgedBrie, Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new() {QualityChange = NormalQuality, Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
                 new() {QualityChange = Legendary, Degrades = false, Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
@@ -63,91 +63,6 @@ namespace GildedRose
                 foreach (Item item in program.Items)
                 {
                     item.Update();
-                }
-            }
-
-        }
-
-        
-        public void UpdateQuality()
-        {
-            for (var i = 0; i < Items.Count; i++)
-            {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            if (Items[i].Name.StartsWith("Conjured"))
-                            {
-                                Items[i].Quality = Items[i].Quality - 2;
-                            }
-                            else
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
                 }
             }
         }
